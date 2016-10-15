5696d1dd24eac4aa1213d08facf448fa24e38be4
UAustin <liuyinggang08@163.com>
2016-10-15 17:11:40 +0800

    second update models.py

--- farming/mysite/models.py
+++ farming/mysite/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 # Create your models here.
 class Farmdata(models.Model):
-	year = models.IntegerField(null =True, blank = True, default=None)
+	year = models.IntegerField(default=None)
 	province = models.CharField(max_length= 20)
 	crop = models.CharField(max_length= 20)
 	type_db = models.CharField(max_length= 20)
-	account = models.FloatField(null =True, blank = True, default=None)
+	account = models.FloatField(default=None)
 
-	def __str__(self):
-		return self.province
+	#def __str__(self):
+		#return self.province
